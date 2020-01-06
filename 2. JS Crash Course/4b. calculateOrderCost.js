0e3284c43c0d98e1ed045e3caccc91ac6f73401e
Jonathan Romano <jonathan@luxaritas.com>
2020-01-06 01:56:33 -0500

    Tweak exercises

--- 2. JS Crash Course/4b. calculateOrderCost.js
+++
@@ -1,7 +0,0 @@
-/*
-Write a function called `calculateOrderCost` that given a weight in pounds and a base price in dollars,
-returns a total cost in dollars. The cost is comprised of the base price, shipping (3 times the weight),
-and tax (1.08 times the base price)
-
-Bonus: Round the price to two decimal places (I'll leave that one up to you!)
-*/
