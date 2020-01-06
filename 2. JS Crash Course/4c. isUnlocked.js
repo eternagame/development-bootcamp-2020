dcc77d84988a0fa88ed74777e75b3cef0dc5f729
Jonathan Romano <jonathan@luxaritas.com>
2020-01-06 01:34:23 -0500

    Add some exercises

---
+++ 2. JS Crash Course/4c. isUnlocked.js
@@ -0,0 +1,5 @@
+/*
+Write a function called `isUnlocked` that given a number of buttons pressed, a codeword (string), and whether or not
+it is a Tuesday, returns a boolean indicating whether the door is unlocked. The number of buttons pressed
+must be greater than 6, the codeword must be "hello", and it must be a Tuesday
+*/
