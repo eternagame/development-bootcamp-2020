9f4c2e8f2bcc8cc10d24c3a061135c01ecda9d43
Jonathan Romano <jonathan@luxaritas.com>
2020-01-07 17:08:02 -0500

    Fix Consistency example

--- 5. Code Quality/01. Consistancy.ts
+++ 5. Code Quality/01. Consistancy.ts
@@ -1,10 +1,12 @@
-//type Point;
+class Point {
+  constructor(public x, public y){};
+}
 
 function addToArray(base_positions, newPosition){
   if(newPosition.y > newPosition.x)
       base_positions.push(newPosition);
   else
-    //base_positions.push(new Point(newPosition.y, newPosition.x))
+    base_positions.push(new Point(newPosition.y, newPosition.x))
 }
 
 // Let eslint make sure your code follows a consistant, readable style
